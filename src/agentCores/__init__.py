1757d24871e06f4c2cee0923047e7cd8538f8d4a
Leo Borcherding <leoleojames1@gmail.com>
2024-12-18 01:40:40 -0600

    agentCores research assistant v0.2.1

--- src/agentCores/__init__.py
+++ src/agentCores/__init__.py
@@ -2,6 +2,7 @@
 from .agentCores import agentCores
 from .agentMatrix import agentMatrix
 from .databaseManager import databaseManager
+from .researchAssistant import researchAssistant
 
 __version__ = "0.1.0"
-__all__ = ['agentCores', 'agentMatrix', 'databaseManager']
+__all__ = ['agentCores', 'agentMatrix', 'databaseManager', 'researchAssistant']
